326eaa5518b45d925c2e1a0e6f744a151cb59743
ishiiyuki <mizuki.yuuri@gmail.com>
2020-08-30 02:42:05 +0900

    スクショ機能追加

---
+++ Assets/Script/HRScreenshot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class HRScreenshot : MonoBehaviour
+{
+    [Header("superSize")]
+    public int superSize = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            string name = "HRimage"+getTimeStamp() + ".png";
+            ScreenCapture.CaptureScreenshot(name, superSize);
+           
+        }
+    }
+
+    private string getTimeStamp()
+    {
+        string time = DateTime.Now.ToString("yyyyMMddHHmmss");
+        return time;
+    }
+}
